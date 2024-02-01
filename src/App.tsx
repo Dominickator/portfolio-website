88f5d0e8bb0b3dc999b7576347acf89f82e0a5c5
Dominick Dickerson <dominickator122102@gmail.com>
2024-01-31 21:04:22 -0600

    Got the barebones structure down hopefully, still some serious bugs to fix.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import ballKnower from './assets/ball-knower.jpg';
+import twoSwords from './assets/two-swords.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="headContainer">
+        <h1>Dominick Dickerson</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='bar'></div>
+      <div className="bodyContainer">
+        <h1>Showcased Projects</h1>
+        <div className="projectPictureContainer">
+          <img src={ballKnower}></img>
+          <img src={twoSwords}></img>
+        </div>
+        <p>Hi there, I am <b>Dominick Dickerson</b>! This website is made to be handed out to employers to show off my handywork! I don't have a ton of huge projects but I do have some great real world experience! Feel free to look around to try and get a feel for who I am!</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
